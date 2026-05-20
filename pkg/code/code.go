548da5fcddfe5a47dfede3d968f838cf039af5e0
L1xiaobai <wsl@DESKTOP-PRIKHKK.>
2026-05-20 17:37:04 +0800

    5.20

--- pkg/code/code.go
+++ pkg/code/code.go
@@ -3,7 +3,7 @@ package code
 const (
 	Success       = 0
 	InvalidParam  = 10001
-	UserNotFound      = 20001
+	UserNotFound  = 20001
 	DatabaseError = 30001
 	RedisError    = 30002
 	InternalError = 50000
